9c811f8c7a07e16633da37049946b29a64fd3c75
Trung Nguyen <trung.nguyen@kyanon.digital>
2017-10-23 18:39:22 +0700

    setup environment for Haml

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,41 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var haml = require('gulp-haml');
+
 gulp.task('sass', function () {
     return gulp.src(['assets/scss/**/*.scss'])
         .pipe(sass()) // Converts Sass to CSS with gulp-sass
         .on('error', onError)
         .pipe(gulp.dest('assets/css'))
 });
+
+// Get one .haml file and render 
+gulp.task('haml-one', function () {
+  gulp.src('./haml/index.haml')
+    .pipe(haml())
+    .pipe(gulp.dest('.'));
+});
+ 
+// Get all .haml files in one folder and render 
+gulp.task('haml', function () {
+  return gulp.src('haml/**/*.haml')
+    .pipe(haml())
+    .pipe(gulp.dest('./html'));
+});
+
 gulp.task('watch', function () {
     gulp.watch('assets/scss/**/*.scss', ['sass']);
     // Other watchers
+    gulp.watch('haml/**/*.haml', ['haml', 'haml-one']);
 });
-
+ 
 gulp.task('default', function () {
-    gulp.watch('assets/scss/**/*.scss', ['sass']);
+    // gulp.watch('assets/scss/**/*.scss', ['sass']);
+    gulp.run('watch');
     // Other watchers
 });
+
+
 function onError(err) {
     console.log(err);
     this.emit('end');
